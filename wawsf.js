d83fcbb90f34035d1a0a38194f10e80bd6536c03
Raphael Lee <Raphael.T.Lee@gmail.com>
2022-01-29 22:03:57 -0800

    Create wawsf.js
    First commit, copying from existing JS in Webflow

---
+++ wawsf.js
@@ -0,0 +1,147 @@
+let countryInterest = {"wishin-and-hopin": {"horoscope": {"Belgium": [32.0, 30.0, 32.0, 41.0, 56.0, 84.0, 79.0, 55.0, 35.0, 22.0, 16.0, 13.0, 13.0, 12.0, 10.0, 10.0, 10.0, 10.0, 10.0, 12.0, 12.0, 14.0, 16.0, 19.0], "Germany": [32.0, 30.0, 32.0, 42.0, 71.0, 87.0, 66.0, 43.0, 28.0, 20.0, 16.0, 13.0, 13.0, 12.0, 11.0, 10.0, 10.0, 11.0, 12.0, 14.0, 13.0, 15.0, 18.0, 21.0], "Mexico": [67.0, 71.0, 71.0, 71.0, 81.0, 84.0, 89.0, 74.0, 58.0, 46.0, 44.0, 36.0, 32.0, 29.0, 28.0, 28.0, 27.0, 28.0, 30.0, 32.0, 34.0, 39.0, 46.0, 57.0], "Russia": [71.0, 71.0, 74.0, 77.0, 83.0, 91.0, 90.0, 77.0, 55.0, 41.0, 36.0, 32.0, 31.0, 30.0, 30.0, 30.0, 31.0, 32.0, 34.0, 37.0, 39.0, 44.0, 49.0, 55.0], "United States": [54.0, 60.0, 65.0, 72.0, 77.0, 83.0, 88.0, 71.0, 55.0, 44.0, 38.0, 33.0, 31.0, 29.0, 28.0, 28.0, 27.0, 27.0, 28.0, 29.0, 33.0, 36.0, 40.0, 47.0]}, "lottery": {"Chile": [31.0, 25.0, 23.0, 26.0, 32.0, 51.0, 58.0, 44.0, 36.0, 31.0, 30.0, 27.0, 30.0, 32.0, 44.0, 31.0, 25.0, 27.0, 39.0, 37.0, 37.0, 46.0, 53.0, 40.0], "France": [21.0, 23.0, 27.0, 33.0, 68.0, 82.0, 49.0, 27.0, 18.0, 14.0, 13.0, 12.0, 10.0, 12.0, 9.0, 9.0, 9.0, 9.0, 10.0, 11.0, 22.0, 28.0, 24.0, 22.0], "Romania": [28.0, 24.0, 27.0, 34.0, 68.0, 89.0, 76.0, 57.0, 41.0, 38.0, 41.0, 42.0, 44.0, 40.0, 45.0, 52.0, 42.0, 42.0, 44.0, 47.0, 50.0, 49.0, 46.0, 41.0], "United States": [36.0, 27.0, 26.0, 32.0, 48.0, 69.0, 77.0, 62.0, 44.0, 32.0, 25.0, 23.0, 26.0, 43.0, 43.0, 36.0, 26.0, 23.0, 22.0, 30.0, 37.0, 30.0, 37.0, 52.0], "South Africa": [25.0, 25.0, 30.0, 38.0, 52.0, 57.0, 55.0, 51.0, 39.0, 32.0, 28.0, 23.0, 22.0, 20.0, 21.0, 19.0, 18.0, 19.0, 20.0, 21.0, 19.0, 49.0, 45.0, 29.0]}, "meditation": {"Belgium": [42.0, 44.0, 36.0, 59.0, 52.0, 68.0, 62.0, 40.0, 27.0, 21.0, 25.0, 18.0, 18.0, 21.0, 19.0, 17.0, 15.0, 15.0, 13.0, 17.0, 22.0, 26.0, 34.0, 47.0], "Germany": [63.0, 65.0, 60.0, 67.0, 78.0, 80.0, 60.0, 44.0, 29.0, 22.0, 21.0, 21.0, 22.0, 24.0, 23.0, 21.0, 20.0, 20.0, 21.0, 23.0, 24.0, 33.0, 46.0, 58.0], "Spain": [55.0, 58.0, 59.0, 57.0, 74.0, 84.0, 84.0, 70.0, 45.0, 28.0, 22.0, 21.0, 19.0, 16.0, 18.0, 24.0, 23.0, 20.0, 18.0, 20.0, 20.0, 23.0, 29.0, 41.0], "Peru": [41.0, 28.0, 35.0, 39.0, 53.0, 86.0, 67.0, 47.0, 29.0, 22.0, 18.0, 18.0, 17.0, 16.0, 16.0, 19.0, 20.0, 20.0, 27.0, 27.0, 24.0, 27.0, 30.0, 32.0], "Pakistan": [36.0, 35.0, 35.0, 33.0, 51.0, 50.0, 67.0, 48.0, 36.0, 42.0, 39.0, 43.0, 36.0, 46.0, 35.0, 40.0, 37.0, 43.0, 39.0, 30.0, 32.0, 39.0, 32.0, 31.0]}, "prayer": {"Greece": [48.0, 52.0, 43.0, 43.0, 55.0, 76.0, 88.0, 64.0, 50.0, 25.0, 18.0, 14.0, 12.0, 13.0, 13.0, 13.0, 14.0, 13.0, 13.0, 14.0, 17.0, 22.0, 28.0, 37.0], "Mexico": [50.0, 44.0, 40.0, 42.0, 48.0, 79.0, 90.0, 70.0, 46.0, 32.0, 23.0, 20.0, 19.0, 17.0, 16.0, 17.0, 16.0, 17.0, 18.0, 20.0, 22.0, 27.0, 39.0, 51.0], "Nigeria": [54.0, 47.0, 56.0, 67.0, 80.0, 88.0, 85.0, 73.0, 55.0, 44.0, 37.0, 33.0, 31.0, 28.0, 26.0, 27.0, 24.0, 25.0, 25.0, 23.0, 24.0, 26.0, 31.0, 41.0], "Philippines": [41.0, 34.0, 34.0, 42.0, 65.0, 95.0, 87.0, 77.0, 67.0, 51.0, 45.0, 38.0, 37.0, 40.0, 39.0, 43.0, 37.0, 43.0, 49.0, 47.0, 51.0, 52.0, 53.0, 47.0], "Russia": [52.0, 48.0, 52.0, 55.0, 59.0, 71.0, 81.0, 73.0, 59.0, 44.0, 35.0, 28.0, 25.0, 23.0, 22.0, 22.0, 23.0, 24.0, 26.0, 29.0, 35.0, 43.0, 51.0, 57.0]}}, "apologies-in-advance": {"the bible": {"Chile": [69.0, 66.0, 60.0, 57.0, 68.0, 64.0, 79.0, 75.0, 64.0, 60.0, 56.0, 53.0, 47.0, 41.0, 41.0, 46.0, 49.0, 54.0, 61.0, 67.0, 77.0, 72.0, 70.0, 70.0], "Colombia": [37.0, 33.0, 36.0, 55.0, 77.0, 85.0, 63.0, 47.0, 38.0, 36.0, 33.0, 32.0, 28.0, 28.0, 30.0, 33.0, 35.0, 36.0, 38.0, 42.0, 43.0, 45.0, 47.0, 42.0], "Ecuador": [36.0, 32.0, 35.0, 46.0, 68.0, 84.0, 73.0, 49.0, 43.0, 39.0, 36.0, 34.0, 30.0, 28.0, 31.0, 34.0, 34.0, 37.0, 37.0, 40.0, 45.0, 42.0, 42.0, 43.0], "Peru": [34.0, 29.0, 30.0, 35.0, 53.0, 72.0, 65.0, 55.0, 41.0, 36.0, 36.0, 34.0, 31.0, 29.0, 29.0, 31.0, 33.0, 35.0, 37.0, 38.0, 40.0, 38.0, 36.0, 36.0], "Taiwan": [41.0, 41.0, 42.0, 41.0, 60.0, 75.0, 84.0, 69.0, 57.0, 51.0, 49.0, 43.0, 32.0, 34.0, 32.0, 34.0, 34.0, 32.0, 30.0, 37.0, 46.0, 47.0, 49.0, 46.0]}, "crossword": {"Australia": [31.0, 25.0, 25.0, 26.0, 36.0, 46.0, 61.0, 77.0, 90.0, 81.0, 74.0, 71.0, 70.0, 74.0, 65.0, 63.0, 62.0, 59.0, 57.0, 55.0, 50.0, 47.0, 43.0, 36.0], "Germany": [15.0, 14.0, 14.0, 17.0, 22.0, 34.0, 49.0, 67.0, 80.0, 77.0, 65.0, 57.0, 54.0, 52.0, 48.0, 46.0, 48.0, 48.0, 45.0, 43.0, 31.0, 27.0, 26.0, 20.0], "Finland": [33.0, 17.0, 49.0, 54.0, 49.0, 55.0, 51.0, 67.0, 57.0, 61.0, 50.0, 47.0, 48.0, 52.0, 45.0, 44.0, 46.0, 55.0, 49.0, 57.0, 48.0, 43.0, 41.0, 41.0], "France": [22.0, 18.0, 16.0, 20.0, 29.0, 41.0, 57.0, 79.0, 84.0, 70.0, 61.0, 62.0, 51.0, 67.0, 65.0, 60.0, 63.0, 71.0, 74.0, 60.0, 52.0, 42.0, 39.0, 32.0], "Israel": [22.0, 18.0, 14.0, 16.0, 22.0, 41.0, 71.0, 83.0, 66.0, 51.0, 42.0, 39.0, 35.0, 35.0, 34.0, 33.0, 36.0, 37.0, 38.0, 34.0, 36.0, 32.0, 27.0, 25.0]}}};
+
+// reverse index: given a term, what is its chart?
+let termToChart = {
+  "horoscope": "wishin-and-hopin",
+  "lottery": "wishin-and-hopin",
+  "meditation": "wishin-and-hopin",
+  "prayer": "wishin-and-hopin",
+  
+  "the bible": "apologies-in-advance",
+  "crossword": "apologies-in-advance",
+  
+};
+
+let accentColorsRBG = {
+	"yellow": "#eebe4c"
+}
+
+
+function highlightClickedTopicLink(clickedTopicLink) {
+  let thisItem = $(clickedTopicLink);
+  let term = thisItem.attr("data-term");
+  let accentColor = thisItem.closest(".artwork").data("accent-color");
+  
+	thisItem.removeClass("underlined-" + accentColor);
+	thisItem.addClass("highlighted-" + accentColor);
+  thisItem.siblings().removeClass("highlighted-" + accentColor);
+  thisItem.siblings().addClass("underlined-" + accentColor);
+
+  thisItem.closest(".analysis").find(".highlighted-term").html(term);
+}
+
+
+function drawAnalysis(clickedTopicLink) {
+	let term = $(clickedTopicLink).data("term");
+  let chart = termToChart[term];
+  let accentColor = $(clickedTopicLink).closest(".artwork").data("accent-color");
+
+  let countries = countryInterest[chart][term];
+
+  // Replace current topic in .highlighted-term
+  $(clickedTopicLink).closest(".analysis").find(".current-topic").text(term);
+  $(clickedTopicLink).closest(".analysis").find(".current-topic").removeClass(["highlighted-yellow", "highlighted-orange", "highlighted-green", "highlighted-blue"]);
+  $(clickedTopicLink).closest(".analysis").find(".current-topic").addClass("highlighted-" + accentColor);
+
+  // Draw sparklines
+  let chartDataContainer = $(clickedTopicLink).closest(".analysis").find(".chart-data-container");
+  chartDataContainer.find(".chart-data").remove();
+  
+  for (let country in countries) {
+    let chartDataDiv = $( "<div/>", {
+	    "class": "chart-data"
+		});
+    
+    // draw country name
+    chartDataDiv.append(
+    	$("<div/>", {
+        "class": "country-name",
+        "text": country
+    	})
+    );
+    
+    // draw sparkline
+    const width = 200;
+    const height = 60;
+    const margin = { top: 1, right: 1, bottom: 1, left: 1 };
+    const boundedwidth = width - margin.left - margin.right;
+    const boundedheight = height - margin.top - margin.bottom;
+
+		const data = countries[country];
+
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, data.length])
+      .range([0, boundedwidth]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data)])
+      .range([boundedheight, 0]);
+
+		let sparklineId = chart + "-" + term.replace(/\s+/g, '-') + "-" + country.replace(/\s+/g, '-').toLowerCase();
+		let countrySparkline = $( "<div/>", {
+	    "class": "country-sparkline",
+      "id": sparklineId
+		});
+    chartDataDiv.append(countrySparkline);
+    chartDataContainer.append(chartDataDiv);
+    
+    const svg = d3
+      .select("#" + sparklineId)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    const line = d3
+      .line()
+      .x((d, i) => xScale(i))
+      .y((d) => yScale(d));
+
+    svg
+      .append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", accentColorsRBG[accentColor])
+      .attr("stroke-width", 2)
+      .attr("d", line);
+  }
+  
+  // move time range label
+
+  let timeOfDay = $(clickedTopicLink).closest(".time-of-day-container").data("time-of-day");
+  $(clickedTopicLink).closest(".analysis").find(".time-range-label").removeClass(["night", "morning", "afternoon", "evening"]);
+	$(clickedTopicLink).closest(".analysis").find(".time-range-label").addClass(timeOfDay);
+
+	$(clickedTopicLink).closest(".analysis").find(".time-range-vertical-lines").removeClass(["height-4", "height-5"]);
+	$(clickedTopicLink).closest(".analysis").find(".time-range-vertical-lines").addClass("height-" + Object.keys(countries).length);
+	$(clickedTopicLink).closest(".analysis").find(".time-of-day-label").text(timeOfDay);  
+}
+
+function setSearchTopics(clickedExplainer) {
+
+  let list = $(clickedExplainer).closest(".artwork").find(".search-topic-list");
+	list.empty();
+  let chartName = $(clickedExplainer).closest(".artwork").data("name");
+
+	for (let topic in countryInterest[chartName]) {
+    let searchTopicElement = $( "<li/>", {
+        "id": "term-" + topic,
+        "data-term": topic,
+        "class": "search-term",
+        "text": topic
+    });
+		list.append(searchTopicElement);
+  }
+  
+  list.find(".search-term").click(function() {
+		highlightClickedTopicLink(this);
+	  drawAnalysis(this);
+	});
+}
+
+$(".explain-the-joke").click(function() {
+	setSearchTopics(this);
+  $(this).closest(".artwork").find(".search-term").first().click();
+});
